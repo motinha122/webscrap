aa5d51ff114642ca33906a77f370d9d47dac17f0
motinha122 <timota132@gmail.com>
2022-07-26 15:30:00 -0300

    chrome binary error

--- scrap.py
+++ scrap.py
@@ -1,12 +1,9 @@
-from selenium.webdriver.chrome.service import Service
 from selenium import webdriver
 
 url = 'https://starwars-visualguide.com/#/characters/1'
 
-service = Service(executable_path='/home/tiago/Documents/webscrap/chromedriver')
-driver = webdriver.Chrome(service=service)
+driver = webdriver.Chrome()
 driver.get(url)
-
 stats = driver.find_element(By.CLASS_NAME, "fs-headline display-block ng-binding")
-
+print(stats)
 driver.quit()
